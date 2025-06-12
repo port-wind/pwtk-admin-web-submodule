966a7c1af87a30a76c8321a6a4a149d818c199cd
yb <>
2025-06-12 10:53:13 +0900

    新增TopNavBar组件及其类型定义，支持导航栏功能并提升组件的类型安全性和可维护性。

---
+++ TopNavBar/index.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts" name="TopNavBar">
+import { defineProps } from 'vue'
+import { NavBar } from 'vant'
+
+const props = defineProps({
+  datas: {
+    type: Object as () => any,
+    required: true
+  }
+})
+const onClickLeft = () => history.back()
+</script>
+
+<template>
+  <div class="top-nav-bar">
+    <div class="top-nav-bar-container">
+      <van-nav-bar :title="datas.configParamJson.title" left-text="返回" left-arrow @click-left="onClickLeft" />
+    </div>
+    <slot name="deles" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.top-nav-bar {
+  position: relative;
+}
+.top-nav-bar-container {
+  min-height: 20px;
+}
+</style>
