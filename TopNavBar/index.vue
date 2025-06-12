335410a783f953ce48339736b135611dc7276ca9
yb <>
2025-06-12 12:42:09 +0900

    更新TopNavBar组件，新增右侧点击事件以实现滚动到顶部的功能，提升用户体验和交互性，同时保持代码的一致性和可读性。

--- TopNavBar/index.vue
+++ TopNavBar/index.vue
@@ -11,7 +11,16 @@ const props = defineProps({
   },
 })
 const onClickLeft = () => history.back()
-
+const onClickRight = () => {
+  // "#com-container" 滚动到顶部
+  const comContainer = document.getElementById('com-container')
+  if (comContainer) {
+    comContainer.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    })
+  }
+}
 const PUBLIC_CDN_URL = 'https://stt.pwtk.cc/'
 // 定义 props 接收父组件传入的数据
 const getFullUrl = (url: string, baseUrl: string): string => {
@@ -29,6 +38,7 @@ const getFullUrl = (url: string, baseUrl: string): string => {
           left-text="返回"
           left-arrow
           @click-left="onClickLeft"
+          @click-right="onClickRight"
         >
           <template #left>
             <Image
