09e7b9ef688603eb33c3b5e3d0d36b9fefbd14ed
yb <>
2025-06-12 11:34:44 +0900

    更新TopNavBar组件，新增logo和右侧箭头图标的支持，优化导航栏的展示效果，同时引入getFullUrl函数以处理图片URL，提升组件的可用性和用户体验。

--- TopNavBar/index.vue
+++ TopNavBar/index.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts" name="TopNavBar">
 import { defineProps } from 'vue'
 import { NavBar } from 'vant'
-
+import { Icon } from 'vant'
+import getImageUrl from '@/utils/getImageUrl'
 const props = defineProps({
   datas: {
     type: Object as () => any,
@@ -9,12 +10,29 @@ const props = defineProps({
   }
 })
 const onClickLeft = () => history.back()
+
+const PUBLIC_CDN_URL = 'https://stt.pwtk.cc/'
+// 定义 props 接收父组件传入的数据
+const getFullUrl = (url: string, baseUrl: string): string => {
+  return /^https?:\/\//.test(url) ? url : `${baseUrl}${url}`
+}
 </script>
 
 <template>
   <div class="top-nav-bar">
     <div class="top-nav-bar-container">
-      <van-nav-bar :title="datas.configParamJson.title" left-text="返回" left-arrow @click-left="onClickLeft" />
+      <van-nav-bar :title="datas.configParamJson.title" left-text="返回" left-arrow @click-left="onClickLeft">
+        <template #left>
+          <van-image
+            class="top-nav-bar-logo"
+            :src="getFullUrl(datas.configParamJson.logo, PUBLIC_CDN_URL)"
+            :alt="datas.configParamJson.title"
+          />
+        </template>
+        <template #right>
+          <van-icon name="arrow-up" size="18" />
+        </template>
+      </van-nav-bar>
     </div>
     <slot name="deles" />
   </div>
@@ -26,5 +44,9 @@ const onClickLeft = () => history.back()
 }
 .top-nav-bar-container {
   min-height: 20px;
+}
+
+.top-nav-bar-logo {
+  height: 100%;
 }
 </style>
