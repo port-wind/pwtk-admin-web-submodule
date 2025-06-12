aeb09b60dfbc36e54aba922fad522e03a69cbce6
yb <>
2025-06-12 11:37:46 +0900

    更新TopNavBar类型定义，移除冗余的itemData和model字段，简化数据结构，同时调整import语法以保持一致性，提升代码可读性和维护性。

--- TopNavBar/type.ts
+++ TopNavBar/type.ts
@@ -1,18 +1,10 @@
-import type { ISetStyle } from "@/views/WebVision/const/componentProperties/index"
+import type { ISetStyle } from '@/views/WebVision/const/componentProperties/index'
 
 export interface IDatas extends ISetStyle<TopNavBar> {
   // configParamJson: CustomBarType
 }
 
 export interface TopNavBar {
-  model: 's1' | 's2' | 's3'
   title?: string
-  itemData: ItemData[]
   logo: string
-}
-
-export interface ItemData {
-  link: string
-  text: string
-  content?: string
 }
