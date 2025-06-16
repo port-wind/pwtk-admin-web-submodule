87b046e43cd46f2170de054551e98a86d9199bd3
yb <>
2025-06-16 11:09:39 +0900

    更新CarouselApp组件，修改了应用标题、推广文本和图标路径，确保内容的准确性和一致性。同时，新增了横幅图像以提升视觉效果，优化了用户体验。

--- componentProperties/CarouselApp.ts
+++ componentProperties/CarouselApp.ts
@@ -26,11 +26,11 @@ componentProperties.set('CarouselApp', {
     configParamJson: {
       tabs: [
         {
-          title: '澳门好彩',
+          title: '特码尽在66853',
           promoText: '特码点击在线',
           amount: '首充送988元',
           bannerColor: '#ff6b6b',
-          appIcon: '/images/app-icons/lottery-app-1.png',
+          appIcon: 'dev/bbs/mainboard/icon/956297c4afb84f3dae0af33bf6149692.png',
           appName: 'tk6图库 APP',
           appSize: '60.28MB',
           category: '图库软件',
@@ -41,14 +41,14 @@ componentProperties.set('CarouselApp', {
           iosDownload: 'https://example.com/ios-download-1',
           webUrl: 'https://example.com/website-1',
           statusText: '已有本站评分9999万，请放心下载！',
-          banner: ''
+          banner: 'dev/bbs/mainboard/icon/92fa75ae0364498288a6c4796f02c457.png'
         },
         {
-          title: '6262彩票',
+          title: '首充送988元',
           promoText: '新朋好友／六含',
           amount: '注册即送188',
           bannerColor: '#4CAF50',
-          appIcon: '/images/app-icons/lottery-app-2.png',
+          appIcon: 'dev/bbs/mainboard/icon/833270c90a73451582a0b93edad7daaa.png',
           appName: '6262彩票 APP',
           appSize: '37.28MB',
           category: '国产软件',
@@ -59,7 +59,7 @@ componentProperties.set('CarouselApp', {
           iosDownload: 'https://example.com/ios-download-2',
           webUrl: 'https://example.com/website-2',
           statusText: '已有本站评分5000万，请放心投注！',
-          banner: ''
+          banner: 'dev/bbs/mainboard/icon/3f28cdcedbab48fe9ada8ab27ba25b98.png'
         }
       ]
     }
