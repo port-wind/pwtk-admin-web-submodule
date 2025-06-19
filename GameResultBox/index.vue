15627547eed78318381401c6ea295d14a40f15f3
yb <>
2025-06-19 09:38:34 +0900

    在GameResultBox组件中引入gameStore以支持游戏类型的动态获取，同时移除冗余的代码和注释，优化了组件的可读性和维护性。此更新确保了组件能够更灵活地适应不同的游戏结果展示需求。

--- GameResultBox/index.vue
+++ GameResultBox/index.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts" name="GameResultBox">
 import { ref, onMounted } from 'vue'
 import { defineEmits } from 'vue'
-// import { globalStore } from '@/store';
 import GameResultModel1 from './GameResultModel1.vue'
 import GameResultModel2 from './GameResultModel2.vue'
 import GameResultModel3 from './GameResultModel3.vue'
@@ -9,6 +8,11 @@ import GameResultModel4 from './GameResultModel4.vue'
 import GameResultModel5 from './GameResultModel5.vue'
 import type { IDatas } from './type'
 import service from '../service/index'
+import { gameStore } from '../store/index'
+import { useStore } from '@nanostores/vue'
+
+const gameStoreData = useStore(gameStore)
+const gameType = computed(() => gameStoreData.value.gameType)
 
 export interface GameResultType {
   isLongName: boolean
@@ -19,7 +23,6 @@ export interface GameResultType {
   isIssue: boolean
   isGifAd: boolean
   showArray: string[]
-  // nameType: string
   noTab: boolean
 }
 
@@ -29,30 +32,10 @@ interface IProps {
 }
 
 const props = defineProps<IProps>()
-// console.log('🚀 ~ props:', props.datas.configParamJson)
-
-// console.log('props', props);
-// console.log('全局变量', window.globalVar); // 输出: This is a global variable
 const emits = defineEmits(['update-issue'])
-// const handleTabClick = (index: number) => {
-//   emits('tab-click', index);
-// };
 
-const handleUpdateIssue = () => {
-  emits('update-issue')
-}
 const rawTabsData = ref([])
-const rawData = ref<GameResultType>({
-  isLongName: false,
-  isIcon: true,
-  model: 's4',
-  isNextIssue: false,
-  isHistory: false,
-  isIssue: true,
-  isGifAd: false,
-  showArray: ['2032', '1', '6', '5'],
-  noTab: false
-})
+
 onMounted(async () => {
   console.log('props', props)
 
@@ -71,228 +54,10 @@ onMounted(async () => {
       ...item,
       ...res[index]
     }))
-    // globalStore.setKey("GameData", tabsData);
-    // componentsData.value = [...componentsData.value];
-    // console.log(' componentsData.value ', componentsData.value);
-    // Set default values
   } catch (error) {
     console.error('请求失败:', error)
   }
 })
-
-const tabsData = ref([
-  {
-    gameType: '2032',
-    gameTypeShortName: '澳彩',
-    gameTypeName: '澳彩',
-    gameTypeLongName: '澳门六合彩',
-    areaCode: 'MAC',
-    classifier: 'official',
-    sortNum: 6,
-    gameTypeCode: 'a6',
-    status: 'y',
-    createTime: 1740632835000,
-    updateTime: 1740632835000,
-    currentCompleteIssue: 2025156,
-    currentIssue: '2025156',
-    currentPeriod: 156,
-    currentResult: [
-      { color: 'B', fiveElements: 'm', index: 1, num: 37, oddEven: 'o', shengxiao: '蛇', size: 'b' },
-      { color: 'R', fiveElements: 'm', index: 2, num: 8, oddEven: 'e', shengxiao: '狗', size: 's' },
-      { color: 'G', fiveElements: 's', index: 3, num: 44, oddEven: 'e', shengxiao: '狗', size: 'b' },
-      { color: 'R', fiveElements: 't', index: 4, num: 19, oddEven: 'o', shengxiao: '猪', size: 's' },
-      { color: 'B', fiveElements: 'j', index: 5, num: 41, oddEven: 'o', shengxiao: '牛', size: 'b' },
-      { color: 'B', fiveElements: 'h', index: 6, num: 10, oddEven: 'e', shengxiao: '猴', size: 's' },
-      { color: 'R', fiveElements: 'm', index: 7, num: 7, oddEven: 'o', shengxiao: '猪', size: 's' }
-    ],
-    currentYear: 2025,
-    data: {
-      beginTime: '2025-06-03 21:32:00',
-      endTime: '2025-06-06 21:32:32',
-      isMaintain: 0,
-      issue: 2025157,
-      lastIssue: 2025156,
-      lotteryId: 2032,
-      lotteryName: '澳門六合彩',
-      originalDataList: [],
-      pet: '蛇',
-      serverTime: '2025-06-03 21:36:00'
-    },
-    lastIssue: 2025155,
-    lastOpenCode: '37,08,44,19,41,10,07',
-    lastResult: [
-      { color: 'B', fiveElements: 'j', index: 1, num: 42, oddEven: 'e', shengxiao: '鼠', size: 'b' },
-      { color: 'R', fiveElements: 'h', index: 2, num: 1, oddEven: 'o', shengxiao: '蛇', size: 's' },
-      { color: 'G', fiveElements: 's', index: 3, num: 21, oddEven: 'o', shengxiao: '鸡', size: 's' },
-      { color: 'B', fiveElements: 'm', index: 4, num: 15, oddEven: 'o', shengxiao: '兔', size: 's' },
-      { color: 'B', fiveElements: 't', index: 5, num: 36, oddEven: 'e', shengxiao: '马', size: 'b' },
-      { color: 'B', fiveElements: 'h', index: 6, num: 47, oddEven: 'o', shengxiao: '羊', size: 'b' },
-      { color: 'G', fiveElements: 't', index: 7, num: 5, oddEven: 'o', shengxiao: '牛', size: 's' }
-    ],
-    lastYear: 2025,
-    nextCompleteIssue: 2025157,
-    nextOpenTime: '2025-06-06 21:32:32',
-    nextPeriod: 157,
-    nextShortIssue: '157',
-    nextYear: 2025
-  },
-  {
-    gameType: '1',
-    gameTypeShortName: '港彩',
-    gameTypeName: '港彩',
-    gameTypeLongName: '香港六合彩',
-    areaCode: 'HKG',
-    classifier: 'official',
-    sortNum: 4,
-    gameTypeCode: 'hk6',
-    status: 'y',
-    createTime: 1740622859000,
-    updateTime: 1740622859000,
-    currentCompleteIssue: 62,
-    currentIssue: '62',
-    currentPeriod: 62,
-    currentResult: [
-      { color: 'G', fiveElements: 'h', index: 1, num: 39, oddEven: 'o', shengxiao: '兔', size: 'b' },
-      { color: 'G', fiveElements: 's', index: 2, num: 22, oddEven: 'e', shengxiao: '猴', size: 's' },
-      { color: 'R', fiveElements: 'h', index: 3, num: 18, oddEven: 'e', shengxiao: '鼠', size: 's' },
-      { color: 'G', fiveElements: 'j', index: 4, num: 33, oddEven: 'o', shengxiao: '鸡', size: 'b' },
-      { color: 'R', fiveElements: 'm', index: 5, num: 24, oddEven: 'e', shengxiao: '马', size: 's' },
-      { color: 'G', fiveElements: 's', index: 6, num: 44, oddEven: 'e', shengxiao: '狗', size: 'b' },
-      { color: 'B', fiveElements: 'j', index: 7, num: 3, oddEven: 'o', shengxiao: '兔', size: 's' }
-    ],
-    currentYear: 2025,
-    data: {
-      lotteryTime: '2025-06-07 21:30:00',
-      nextLotteryNumber: '063',
-      nextLotteryTimestamp: 1749299400000,
-      originalDataList: ['39', '22', '18', '33', '24', '44', '03'],
-      period: '062'
-    },
-    lastIssue: 62,
-    lastResult: [
-      { color: 'G', fiveElements: 'h', index: 1, num: 39, oddEven: 'o', shengxiao: '兔', size: 'b' },
-      { color: 'G', fiveElements: 's', index: 2, num: 22, oddEven: 'e', shengxiao: '猴', size: 's' },
-      { color: 'R', fiveElements: 'h', index: 3, num: 18, oddEven: 'e', shengxiao: '鼠', size: 's' },
-      { color: 'G', fiveElements: 'j', index: 4, num: 33, oddEven: 'o', shengxiao: '鸡', size: 'b' },
-      { color: 'R', fiveElements: 'm', index: 5, num: 24, oddEven: 'e', shengxiao: '马', size: 's' },
-      { color: 'G', fiveElements: 's', index: 6, num: 44, oddEven: 'e', shengxiao: '狗', size: 'b' },
-      { color: 'B', fiveElements: 'j', index: 7, num: 3, oddEven: 'o', shengxiao: '兔', size: 's' }
-    ],
-    lastYear: 2025,
-    nextCompleteIssue: 63,
-    nextIssue: '62',
-    nextOpenTime: '2025-06-07 21:30:00',
-    nextPeriod: 63,
-    nextShortIssue: '063',
-    nextYear: 2025
-  },
-  {
-    gameType: '6',
-    gameTypeShortName: '快乐8',
-    gameTypeName: '快乐8',
-    gameTypeLongName: '快乐8',
-    areaCode: 'MAC1',
-    classifier: 'official',
-    sortNum: 3,
-    gameTypeCode: 'kl8',
-    status: 'y',
-    createTime: 1740461165000,
-    updateTime: 1740461165000,
-    currentCompleteIssue: 2025146,
-    currentIssue: '2025146',
-    currentPeriod: 146,
-    currentResult: [
-      { color: 'G', fiveElements: 'h', index: 1, num: 17, oddEven: 'o', shengxiao: '牛', size: 's' },
-      { color: 'R', fiveElements: 'j', index: 2, num: 12, oddEven: 'e', shengxiao: '马', size: 's' },
-      { color: 'B', fiveElements: 'h', index: 3, num: 10, oddEven: 'e', shengxiao: '猴', size: 's' },
-      { color: 'G', fiveElements: 'j', index: 4, num: 33, oddEven: 'o', shengxiao: '鸡', size: 'b' },
-      { color: 'G', fiveElements: 't', index: 5, num: 28, oddEven: 'e', shengxiao: '虎', size: 'b' },
-      { color: 'R', fiveElements: 'm', index: 6, num: 46, oddEven: 'e', shengxiao: '猴', size: 'b' },
-      { color: 'G', fiveElements: 's', index: 7, num: 22, oddEven: 'e', shengxiao: '猴', size: 's' }
-    ],
-    currentYear: 2025,
-    data: {
-      lotteryTime: '2025-06-06 21:30:00',
-      nextLotteryNumber: '147',
-      nextLotteryTimestamp: 1749213000000,
-      originalDataList: ['17', '12', '10', '33', '28', '46', '22'],
-      period: '146'
-    },
-    lastIssue: 2025146,
-    lastResult: [
-      { color: 'G', fiveElements: 'h', index: 1, num: 17, oddEven: 'o', shengxiao: '牛', size: 's' },
-      { color: 'R', fiveElements: 'j', index: 2, num: 12, oddEven: 'e', shengxiao: '马', size: 's' },
-      { color: 'B', fiveElements: 'h', index: 3, num: 10, oddEven: 'e', shengxiao: '猴', size: 's' },
-      { color: 'G', fiveElements: 'j', index: 4, num: 33, oddEven: 'o', shengxiao: '鸡', size: 'b' },
-      { color: 'G', fiveElements: 't', index: 5, num: 28, oddEven: 'e', shengxiao: '虎', size: 'b' },
-      { color: 'R', fiveElements: 'm', index: 6, num: 46, oddEven: 'e', shengxiao: '猴', size: 'b' },
-      { color: 'G', fiveElements: 's', index: 7, num: 22, oddEven: 'e', shengxiao: '猴', size: 's' }
-    ],
-    lastYear: 2025,
-    nextCompleteIssue: 2025147,
-    nextIssue: '145',
-    nextOpenTime: '2025-06-06 21:30:00',
-    nextPeriod: 147,
-    nextShortIssue: '147',
-    nextYear: 2025
-  },
-  {
-    gameType: '5',
-    gameTypeShortName: '新澳彩',
-    gameTypeName: '新澳彩',
-    gameTypeLongName: '新澳门六合彩',
-    areaCode: 'NMAC',
-    classifier: 'official',
-    sortNum: 2,
-    gameTypeCode: 'xa6',
-    status: 'y',
-    createTime: 1740461173000,
-    updateTime: 1740461173000,
-    currentCompleteIssue: 2025156,
-    currentIssue: '2025156',
-    currentPeriod: 156,
-    currentResult: [
-      { color: 'B', fiveElements: 'm', index: 1, num: 15, oddEven: 'o', shengxiao: '兔', size: 's' },
-      { color: 'R', fiveElements: 's', index: 2, num: 13, oddEven: 'o', shengxiao: '蛇', size: 's' },
-      { color: 'G', fiveElements: 'm', index: 3, num: 38, oddEven: 'e', shengxiao: '龙', size: 'b' },
-      { color: 'G', fiveElements: 'h', index: 4, num: 17, oddEven: 'o', shengxiao: '牛', size: 's' },
-      { color: 'G', fiveElements: 't', index: 5, num: 28, oddEven: 'e', shengxiao: '虎', size: 'b' },
-      { color: 'G', fiveElements: 'h', index: 6, num: 32, oddEven: 'e', shengxiao: '狗', size: 'b' },
-      { color: 'R', fiveElements: 'h', index: 7, num: 18, oddEven: 'e', shengxiao: '鼠', size: 's' }
-    ],
-    currentYear: 2025,
-    data: { originalDataList: [] },
-    k: '2025156,15,13,38,17,28,32,18,2025157,06,06,五,21点32分',
-    lastIssue: 2025156,
-    lastResult: [
-      { color: 'B', fiveElements: 'm', index: 1, num: 15, oddEven: 'o', shengxiao: '兔', size: 's' },
-      { color: 'R', fiveElements: 's', index: 2, num: 13, oddEven: 'o', shengxiao: '蛇', size: 's' },
-      { color: 'G', fiveElements: 'm', index: 3, num: 38, oddEven: 'e', shengxiao: '龙', size: 'b' },
-      { color: 'G', fiveElements: 'h', index: 4, num: 17, oddEven: 'o', shengxiao: '牛', size: 's' },
-      { color: 'G', fiveElements: 't', index: 5, num: 28, oddEven: 'e', shengxiao: '虎', size: 'b' },
-      { color: 'G', fiveElements: 'h', index: 6, num: 32, oddEven: 'e', shengxiao: '狗', size: 'b' },
-      { color: 'R', fiveElements: 'h', index: 7, num: 18, oddEven: 'e', shengxiao: '鼠', size: 's' }
-    ],
-    lastYear: 2025,
-    nextCompleteIssue: 2025157,
-    nextOpenTime: '2025-06-06 21:32:32',
-    nextPeriod: 157,
-    nextShortIssue: '157',
-    nextYear: 2025
-  }
-])
-
-// const data = ref({
-//   model: 's1' as 's1' | 's2' | 's3',
-//   isHistory: false,
-//   isNextIssue: false,
-//   isIssue: true,
-//   isIcon: true,
-//   isLongName: false,
-//   isGifAd: false,
-//   noTab: false,
-//   showArray: ['2032', '1', '84', '5', '3995']
-// })
 </script>
 
 <template>
