ff2277e46507c494cfd6a57878951325aa62fd1c
yb <>
2025-06-24 09:40:00 +0900

    新增多个组件，包括auxiliarysegmentation、basicmodule、captiontext、commoditysearch、communitypowder、crowdoperation、custommodule、entertheshop、follow、graphicnavigation、magiccube、notice、onlineservice、personalizedrecommendation、pictureads、placementarea、richtext、storeinformation、suspension、tabBar、videoss和visionPost。每个组件均包含基本的模板、样式和属性定义，提升了项目的可扩展性和组件复用性。这一更改为后续功能开发奠定了基础。

---
+++ notice/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="notice">
+    <!-- 公告 -->
+    <van-notice-bar
+      :text="datas.noticeText"
+      left-icon="volume-o"
+      :background="datas.backColor"
+      :color="datas.textColor"
+    />
+
+    <!-- 删除组件 -->
+    <slot name="deles" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { NoticeBar as vanNoticeBar } from 'vant'
+import { ComponentStyle } from './type'
+
+const props = defineProps({
+  datas: { type: Object as PropType<ComponentStyle>, required: true }
+})
+// console.log('notice组件', props.datas)
+// export default {
+//   name: 'notice',
+//   props: {
+//     datas: Object
+//   }
+// }
+</script>
+
+<style scoped lang="less">
+.notice {
+  position: relative;
+}
+</style>
---
+++ notice/type.ts
@@ -0,0 +1,6 @@
+export interface ComponentStyle {
+  name: string
+  noticeText: string // 或者更具体的类型
+  backColor: string
+  textColor: string
+}
