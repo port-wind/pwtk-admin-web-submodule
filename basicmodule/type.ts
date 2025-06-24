e39fc58186ee6be9845539e0f5af8ce6fcd00bbd
yb <>
2025-06-24 16:30:56 +0900

    删除多个组件及其相关类型定义，进一步优化项目结构，减少冗余代码，提升代码的可维护性和清晰度，为后续开发奠定基础。

--- basicmodule/type.ts
+++
@@ -1,4 +0,0 @@
-export interface ComponentStyle {
-  name: string
-  json: Record<string, any> // 或者更具体的类型
-}
--- notice/type.ts
+++
@@ -1,6 +0,0 @@
-export interface ComponentStyle {
-  name: string
-  noticeText: string // 或者更具体的类型
-  backColor: string
-  textColor: string
-}
