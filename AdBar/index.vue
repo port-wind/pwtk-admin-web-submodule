8c87533cbf96b775cdf99a85c8739aafbe208a7d
yb <>
2025-07-16 17:15:21 +0900

    修复AdBar组件标题显示逻辑，移除不必要的display条件判断，确保标题始终可见以提升用户体验和组件可维护性。

--- AdBar/index.vue
+++ AdBar/index.vue
@@ -2,7 +2,7 @@
   <div class="AdBar-container">
     <div class="ad-bar-container">
       <!-- 头部标题 -->
-      <div v-if="!display" class="_component-title">{{ datas.componentName }}bar -- 并不显示在页面上</div>
+      <div class="_component-title">{{ datas.componentName }}bar -- 并不显示在页面上</div>
       <div
         class="mbox"
         data-component-name="广告大全"
@@ -21,7 +21,7 @@
 </template>
 
 <script setup lang="ts" name="AdBar">
-const display = import.meta.env.PUBLIC_DISPLAY
+// const display = import.meta.env.PUBLIC_DISPLAY
 
 import type { IDatas } from './type'
 interface IProps {
